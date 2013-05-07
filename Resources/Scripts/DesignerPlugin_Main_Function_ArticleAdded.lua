6e93e4e21969eb2001b49ed895f543289c411a66
Lars Juel Jensen <lars.jensen@asp-as.no>
2013-05-07 10:43:19 +0200

    Work in progress. Log removal and code cleanup.

--- Resources/Scripts/DesignerPlugin_Main_Function_ArticleAdded.lua
+++ Resources/Scripts/DesignerPlugin_Main_Function_ArticleAdded.lua
@@ -12,7 +12,8 @@ function DesignerPlugin_Main.ArticleAdded ( sID, sArticleNumber )
     if sArticleNumber ~= nil then
         local sArticleInfo =  hashtable.get( this.htArticleCache ( ), sArticleNumber ) 
         this.LogToWeb ( "ArticleAdded - "..sArticleInfo )
-        system.openURL ( "javascript:onArticleAdded("..sID..", '"..sArticleInfo.."');", "" )
+        log.message ( "ArticleAdded - "..sArticleInfo )
+        system.openURL ( "javascript:onArticleAdded('"..sID.."', '"..sArticleInfo.."');", "" )
     end
 --------------------------------------------------------------------------------
 end
--- Resources/Scripts/MovingObject_Handler_onMouseButtonUp.lua
+++ Resources/Scripts/MovingObject_Handler_onMouseButtonUp.lua
@@ -8,63 +8,47 @@
 function MovingObject.onMouseButtonUp ( nButton, nPointX, nPointY, nRayPntX, nRayPntY, nRayPntZ, nRayDirX, nRayDirY, nRayDirZ )
 --------------------------------------------------------------------------------
 	 
- local hUser = application.getCurrentUser ( )
- local hScene = application.getCurrentUserScene ( )
- local hUnderCursorComponent = hud.getUnderCursorComponent ( hUser )
- local hTrash = hud.getComponent ( hUser, "MoveObjectHUD.Trash" )
- local sTag1, sTag2
- this.bListSelection ( false )
+    local hUser = application.getCurrentUser ( )
+    local hScene = application.getCurrentUserScene ( )
+    local hUnderCursorComponent = hud.getUnderCursorComponent ( hUser )
+    local hTrash = hud.getComponent ( hUser, "MoveObjectHUD.Trash" )
+    local sTag1, sTag2
 
+    this.bListSelection ( false ) 
+    this.bMouseDown( false ) 
  
- this.bMouseDown( false ) 
+    hashtable.empty ( this.htChildrenY ( ) )
  
- --hashtable.empty ( this.htChildren ( ) )
- hashtable.empty ( this.htChildrenY ( ) )
- 
- if hUnderCursorComponent then
-     sTag1 = hud.getComponentTag ( hUnderCursorComponent )
-     --log.message ("sTag1: ", sTag1  )
-     if hTrash then      
-        sTag2 = hud.getComponentTag ( hTrash )  
+    if hUnderCursorComponent then
+        sTag1 = hud.getComponentTag ( hUnderCursorComponent )
+
+        if hTrash then      
+            sTag2 = hud.getComponentTag ( hTrash )  
             local nResult = string.compare ( sTag1,sTag2 )
             if nResult ~= 0 then
             else
             this.DestroyObject (  )
             end
-     end
-end
+        end
+    end
       
 
-        if ( application.getTotalFrameTime ( ) - this.nLatestTapTime ( ) < this.nTapMaximumDelay ( ) )
-        then
-          this.postEvent ( this.nTapMaximumDelay ( ), "onTapAction", this.nLatestTapTime ( ), this.nTapCount ( ) )
-        end 
+    if ( application.getTotalFrameTime ( ) - this.nLatestTapTime ( ) < this.nTapMaximumDelay ( ) ) then
+        this.postEvent ( this.nTapMaximumDelay ( ), "onTapAction", this.nLatestTapTime ( ), this.nTapCount ( ) )
+    end 
         
-        if hUnderCursorComponent --== hTrash
-        then
+    if this.hMovingObject ( ) ~= nil then
 
-        local sTag = hud.getComponentTag ( hUnderCursorComponent )
-        end
+        local sObjectTag = scene.getObjectTag ( hScene, this.hMovingObject() )
+        this.RecordAction( sObjectTag, 2 )
+    end
 
-log.message( "hMovingObject = ", this.hMovingObject () ) 
-if this.hMovingObject ( ) ~= nil then
+    this.hNewObject ( nil )
+    this.hMovingObject ( nil )
+    this.TargetIsVisible ( false )
+    this.bListSelection ( false )
 
-local sObjectTag = scene.getObjectTag ( hScene, this.hMovingObject() )
-
-this.RecordAction( sObjectTag, 2 )
-end
-
-
-
-this.hNewObject ( nil )
-this.hMovingObject ( nil )
-this.TargetIsVisible ( false )
-this.bListSelection ( false )
-
-this.Idle ( )
-
-
-
+    this.Idle ( )
 ---------------------------------------------------------------------------------
 end
 --------------------------------------------------------------------------------
