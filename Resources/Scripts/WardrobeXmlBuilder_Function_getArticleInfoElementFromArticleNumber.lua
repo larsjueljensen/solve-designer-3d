5092f9e6fbcfeba1444b2ac4d73b06fe4be0937d
Lars Juel Jensen <lars.jensen@asp-as.no>
2013-05-08 19:47:24 +0200

    Added building of wardrobe xml

---
+++ Resources/Scripts/WardrobeXmlBuilder_Function_getArticleInfoElementFromArticleNumber.lua
@@ -0,0 +1,18 @@
+--------------------------------------------------------------------------------
+--  Function......... : getArticleInfoElementFromArticleNumber
+--  Author........... : Lars Juel Jensen
+--  Description...... : Returns the cached <articleInfo> document given it's article number
+--------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------
+function WardrobeXmlBuilder.getArticleInfoElementFromArticleNumber ( sArticleNumber )
+--------------------------------------------------------------------------------
+	
+    local hArticleInfoElement = xml.newInstance ( )
+    local htArticleInfoCache = user.getAIVariable ( this.getUser ( ), "DesignerPlugin_Main", "htArticleCache" )
+    xml.createFromString ( hArticleInfoElement, hashtable.get ( htArticleInfoCache, sArticleNumber ) )
+    return xml.getRootElement ( hArticleInfoElement )
+	
+--------------------------------------------------------------------------------
+end
+--------------------------------------------------------------------------------
